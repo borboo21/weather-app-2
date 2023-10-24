7b07dc0e1c9af74e1b35a32a742f8d1498581607
borboo21 <nikich621@gmail.com>
2023-10-24 23:32:26 +0300

    Осуществили логику серчбара (расширение при вводе в инпут); Поправил стили (сделал theme, с помощью флексов, отцентровал).

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,35 +1,55 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Stack, TextField, Typography} from "@mui/material";
 import theme from "../../theme";
 
 export function SearchBar() {
+
+    const [value, setValue] = useState("");
+    const [width, setWidth] = useState(0);
+    const ref = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const el = ref.current as HTMLElement;
+        if (!el) return;
+        setWidth(el.offsetWidth)
+    },[value])
+
     return(
-        <Stack sx={{
-            direction: "row",
+        <Stack direction="row"
+            sx={{
             justifyContent: "center",
             fontWeight: "medium",
             alignItems: "center",
-            margin: theme.spacing(1),
-            marginTop: theme.spacing(25),
         }}>
             <Typography id="weatherStatus"
                 sx={{
                     fontSize: "h6.fontSize",
-                    letterSpacing: theme.spacing(1),
+                    letterSpacing: theme.spacing(),
             }}>Right now in
             <TextField id="searchInput"
                        variant="standard"
+                       onChange={(e) => {
+                           setValue(e.target.value)
+                       }}
                        sx={{
                            input: {textAlign: "center",
                            fontSize: "h6.fontSize",
-                           padding: theme.spacing(0.25),
+                           padding: theme.spacing(0.5),
                            letterSpacing: theme.spacing(0.25),
                            color: theme.palette.primary.main},
-                           width: theme.spacing(15),
+                           minWidth: theme.spacing(15),
+                           width: `${width + 8}px`,// + paddings
+                           maxWidth: theme.spacing(100),
                            marginX: theme.spacing(0.5),
                        }}
                         ></TextField>
                 ,good weather</Typography>
+            <Stack ref={ref}
+            sx={{
+                position:"absolute",
+                opacity:0,
+                pointerEvents: "none"
+            }}>{value}</Stack>
         </Stack>
     )
 }
