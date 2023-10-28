34a618c54eb02a4675d9c29174eb799de330ce03
arkady <arkadychakhalyan@gmail.com>
2023-10-28 15:58:32 +0400

    3: refactor

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,57 +1,106 @@
-import React, {useEffect, useRef, useState} from "react";
-import {Stack, TextField, Typography} from "@mui/material";
-import theme from "../../theme";
+import React, { useEffect, useRef, useState } from 'react';
+import { Stack, TextField, Typography } from '@mui/material';
+import theme from '../../theme';
 
 export function SearchBar() {
 
     const [value, setValue] = useState("");
     const [width, setWidth] = useState(0);
-    const ref = useRef<HTMLDivElement | null>(null);
+    const [maxWidth, setMaxWidth] = useState(0);
+    const falseInputRef = useRef<HTMLDivElement | null>(null);
+    const textStartRef = useRef<HTMLDivElement | null>(null);
+    const textEndRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
-        const el = ref.current as HTMLElement;
+        const el = falseInputRef.current as HTMLElement;
         if (!el) return;
-        setWidth(el.offsetWidth)
-    },[value])
+        setWidth(el.offsetWidth);
+    },[value]);
+
+    React.useEffect(() => {
+        const onResize = () => {
+            const textStartEl = textStartRef?.current?.offsetWidth || 0;
+            const textEndEl = textEndRef?.current?.offsetWidth || 0;
+            const padding = parseFloat(theme.spacing(6));
+            const maxWidth = window.innerWidth < theme.breakpoints.values.sm ?
+                parseFloat(theme.spacing(30))
+                : window.innerWidth - textStartEl - textEndEl - padding
+            setMaxWidth(maxWidth);
+        };
+
+        onResize();
+
+        window.addEventListener('resize', onResize);
+        return () => window.removeEventListener('resize', onResize);
+    }, []);
 
     return(
-        <Stack direction="row"
+        <Stack
+            direction="row"
             sx={{
-            justifyContent: "center",
-            fontWeight: "medium",
-            alignItems: "center",
-        }}>
-            <Typography id="weatherStatus"
+                justifyContent: "center",
+                fontWeight: "medium",
+                alignItems: "center",
+                gap: theme.spacing(0.5),
+                [theme.breakpoints.down('sm')]: {
+                    flexDirection: 'column'
+                }
+            }}
+        >
+            <Typography
+                ref={textStartRef}
+                id="weatherStatus"
+                sx={textStyle}
+            >
+                Right now in
+            </Typography>
+            <TextField
+                id="searchInput"
+                variant="standard"
+                onChange={(e) => {
+                    setValue(e.target.value)
+                }}
                 sx={{
-                    fontSize: "h6.fontSize",
-                    letterSpacing: theme.spacing(),
-            }}>Right now in
-            <TextField id="searchInput"
-                       variant="standard"
-                       onChange={(e) => {
-                           setValue(e.target.value)
-                       }}
-                       sx={{
-                           input: {textAlign: "center",
-                           fontSize: "h6.fontSize",
-                           padding: theme.spacing(0.5),
-                           letterSpacing: theme.spacing(0.25),
-                           color: theme.palette.primary.main},
-                           minWidth: theme.spacing(15),
-                           width: `${width + 8}px`,// + paddings
-                           maxWidth: theme.spacing(100),
-                           marginX: theme.spacing(0.5),
-                       }}
-                        ></TextField>
-                ,good weather</Typography>
-            <Stack ref={ref}
-            sx={{
-                position:"absolute",
-                opacity:0,
-                pointerEvents: "none"
-            }}>{value}</Stack>
+                    input: {
+                        ...inputStyle,
+                        color: theme.palette.primary.main,
+                        textAlign: "center"
+                    },
+                    minWidth: theme.spacing(15),
+                    width,
+                    maxWidth,
+                }}
+            />
+            <Typography
+                ref={textEndRef}
+                id="weatherStatus"
+                sx={textStyle}
+            >
+                ,good weather
+            </Typography>
+            <Stack
+                ref={falseInputRef}
+                sx={{
+                    ...inputStyle,
+                    position: "absolute",
+                    opacity: 0,
+                    pointerEvents: "none",
+                }}>
+                {value}
+            </Stack>
         </Stack>
     )
 }
 
-export default SearchBar;
+const textStyle = {
+    fontSize: "h6.fontSize",
+    letterSpacing: theme.spacing(),
+    whiteSpace: 'nowrap'
+};
+
+const inputStyle = {
+    p: theme.spacing(0.5),
+    fontFamily: theme.typography.fontFamily,
+    fontSize: "h6.fontSize",
+    letterSpacing: theme.spacing(0.25),
+}
